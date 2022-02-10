898401dbc76377a3c527b2471ac9748218990704
Stefanie Buntz <stefaniebuntz@Stefanies-MacBook-Pro.local>
2022-02-10 19:38:45 +1030

    Updates

---
+++ precache-manifest.e6f06d73b796319cc8b717c77d6b6030.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddf4a181917a913843500e71b5e633a",
+    "url": "/Sb-Portfolio/index.html"
+  },
+  {
+    "revision": "92469205c1b365120b5f",
+    "url": "/Sb-Portfolio/static/css/2.f784d1b5.chunk.css"
+  },
+  {
+    "revision": "17521b9f8db2ee1b97c8",
+    "url": "/Sb-Portfolio/static/css/main.85e649c0.chunk.css"
+  },
+  {
+    "revision": "92469205c1b365120b5f",
+    "url": "/Sb-Portfolio/static/js/2.7a9c8ad6.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Sb-Portfolio/static/js/2.7a9c8ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17521b9f8db2ee1b97c8",
+    "url": "/Sb-Portfolio/static/js/main.8e426bd0.chunk.js"
+  },
+  {
+    "revision": "3556d2aa3fbab22a26b1",
+    "url": "/Sb-Portfolio/static/js/runtime-main.bf20df1d.js"
+  },
+  {
+    "revision": "ab850eb3db11dedb00a936111a18b763",
+    "url": "/Sb-Portfolio/static/media/404.ab850eb3.png"
+  },
+  {
+    "revision": "4ee92646e8db219522099784ce646231",
+    "url": "/Sb-Portfolio/static/media/about.4ee92646.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Sb-Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Sb-Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Sb-Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Sb-Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Sb-Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e25ee348c2b1e9d34bb9b174a549f97b",
+    "url": "/Sb-Portfolio/static/media/heroVideo.e25ee348.mp4"
+  },
+  {
+    "revision": "e1769a6d3b352a9571475c4895e34a7c",
+    "url": "/Sb-Portfolio/static/media/portfolio-1.e1769a6d.jpg"
+  },
+  {
+    "revision": "9c294de417aef6ecbd1af39cf8b7f768",
+    "url": "/Sb-Portfolio/static/media/portfolio-2.9c294de4.jpg"
+  },
+  {
+    "revision": "e2268732e83d5a1c484947be32217c49",
+    "url": "/Sb-Portfolio/static/media/portfolio-3.e2268732.jpg"
+  },
+  {
+    "revision": "99609e33f33ad1f9a06a0cbc8e90b45a",
+    "url": "/Sb-Portfolio/static/media/portfolio-4.99609e33.jpg"
+  },
+  {
+    "revision": "7f619ed0cd9e8cd5cc96165a1143eb21",
+    "url": "/Sb-Portfolio/static/media/portfolio-5.7f619ed0.jpg"
+  },
+  {
+    "revision": "0f87bf717cb6c877a76fc599b06c8393",
+    "url": "/Sb-Portfolio/static/media/portfolio-6.0f87bf71.jpg"
+  }
+]);
